18f517534400289de2f2b8b6bc5fc2d39b145d53
gamejoon <gamejoon0325@naver.com>
2023-10-01 04:03:16 +0900

    create boy_tracing_mouse_click.py

---
+++ boy_tracing_mouse_click.py
@@ -0,0 +1,16 @@
+from pico2d import *
+
+TUK_WIDHT, TUK_HEIGHT = 1280, 1024
+
+open_canvas(TUK_WIDHT, TUK_HEIGHT)
+
+background = load_image("TUK_GROUND.png")
+arrow = load_image("hand_arrow.png")
+character = load_image("run_animation.png")
+
+running = True
+
+while running:
+    pass
+
+close_canvas()
